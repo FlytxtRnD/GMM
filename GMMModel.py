ccad5fe779c2533ea23b0190134f96bc98112ee4
FlytxtRnD <meethu.mathew@flytxt.com>
2014-11-25 13:08:07 +0530

    Accepts both Dense and Sparse Vector inputs

--- GMMModel.py
+++ GMMModel.py
@@ -1,40 +1,43 @@
-
 import numpy as np
 from pyspark import SparkContext
 from GMMclustering import GMMclustering
-from pyspark.mllib.linalg import SparseVector
-from pyspark.mllib._common import \
-    _get_unmangled_rdd, _get_unmangled_double_vector_rdd
+from pyspark.mllib.linalg import Vectors, SparseVector
+
 
 class GMMModel(object):
     """
     A clustering model derived from the Gaussian Mixture model.
 
-    >>> data = np.array([0.3,0.8,-0.58,0.75,-1,-0.40 ,0.70,-0.20,0.25,0.50,\
-        -0.85,0.45,-0.55,-0.75,0.75,-1]).reshape(8,2)
-    >>> model = GMMModel.trainGMM(sc.parallelize(data),4,10)
-    >>> np.argmax(model.predict(np.array([0.3,0.8]))) == \
-        np.argmax(model.predict(np.array([0.25,0.50])))
+    >>> data = sc.parallelize(np.array([0.5,1,0.75,1,-0.75,0.5,-0.5,0.5,\
+        -1,-0.5,-0.75,-0.75,0.75,-0.5,0.75,-0.75]).reshape(8,2))
+    >>> model = GMMModel.trainGMM(data,4,10)
+    >>> np.argmax(model.predict(np.array([0.5,1]))) == \
+        np.argmax(model.predict(np.array([0.75,1])))
     True
-    >>> np.argmax(model.predict(np.array([-0.58,0.75]))) == \
-        np.argmax(model.predict(np.array([-0.85,0.45])))
+    >>> np.argmax(model.predict(np.array([-0.75,0.5]))) == \
+        np.argmax(model.predict(np.array([-0.5,0.5])))
     True
-    >>> np.argmax(model.predict(np.array([-1,-0.40]))) == \
-        np.argmax(model.predict(np.array([0.70,-0.20])))
+    >>> np.argmax(model.predict(np.array([-1,-0.5]))) == \
+        np.argmax(model.predict(np.array([0.75,-0.5])))
     False
-    >>> np.argmax(model.predict(np.array([0.75,-1]))) == \
-        np.argmax(model.predict(np.array([-0.85,0.45])))
+    >>> np.argmax(model.predict(np.array([0.75,-0.75]))) == \
+        np.argmax(model.predict(np.array([-0.75,-0.75])))
     False
-    >>> sparse_data = [
-    ...     SparseVector(3, {1: 1.0}),
-    ...     SparseVector(3, {1: 1.1}),
-    ...     SparseVector(3, {2: 1.0}),
-    ...     SparseVector(3, {2: 1.1})
-    ... ]
-    >>> sparse_data_rdd = sc.parallelize(sparse_data).map(sparseToArray)
+
+    >>> sparse_data = ([Vectors.sparse(3, {1: 1.0}),\
+                    Vectors.sparse(3, {1: 1.1}),\
+                    Vectors.sparse(3, {2: 1.0}),\
+                    Vectors.sparse(3, {2: 1.1})])
+    >>> sparse_data_rdd = sc.parallelize(sparse_data)
     >>> model = GMMModel.trainGMM(sparse_data_rdd,2,10)
     >>> np.argmax(model.predict(np.array([0., 1., 0.]))) == \
         np.argmax(model.predict(np.array([0, 1.1, 0.])))
+    True
+    >>> np.argmax(model.predict(Vectors.sparse(3, {1: 1.0}))) == \
+        np.argmax(model.predict(Vectors.sparse(3, {2: 1.0})))
+    False
+    >>> np.argmax(model.predict(sparse_data[2])) == \
+        np.argmax(model.predict(sparse_data[3]))
     True
     """
 
@@ -43,7 +46,6 @@ class GMMModel(object):
         """
         Train a GMM clustering model.
         """
-        # data = _get_unmangled_double_vector_rdd(data)
         gmmObj = GMMclustering().fit(data, k, n_iter)
         return gmmObj
 
@@ -53,18 +55,10 @@ class GMMModel(object):
         Get the result of predict
         Return responsibility matrix and cluster labels .
         """
-        # predictRdd = data.map(lambda y:gmmObj.predict(y)
-        # responsibility_matrix = predictRdd.map(lambda c: c[0]).collect()
-        # cluster_labels = predictRdd.map(lambda c: c[1]).collect()
         responsibility_matrix = data.map(lambda m: gmmObj.predict(m))
         cluster_labels = responsibility_matrix.map(lambda b: np.argmax(b))
-        return responsibility_matrix,cluster_labels
+        return responsibility_matrix, cluster_labels
 
-def sparseToArray(sparsevector):
-        arr = np.zeros(sparsevector.size) 
-        for i in xrange(sparsevector.indices.size): 
-            arr[sparsevector.indices[i]] = sparsevector.values[i] 
-        return arr 
 
 def _test():
     import doctest
--- PyGMM.py
+++ PyGMM.py
@@ -5,9 +5,8 @@ to estimate the parameters.
 """
 import sys
 import numpy as np
-from pyspark import SparkContext
 from GMMModel import GMMModel
-from matplotlib import pyplot as plt
+from pyspark import SparkContext
 
 
 def parseVector(line):
@@ -25,12 +24,13 @@ if __name__ == "__main__":
     if len(sys.argv) < 4:
         print >> sys.stderr, (
             "Invalid number of arguments ::  Usage: "
-            "GMM <master> <input_file> <num_of_clusters>[<num_of_iterations>]")
+            "PyGMM <master> <input_file> <num_of_clusters>[<num_of_iterations>]")
         exit(-1)
     if not(sys.argv[1].startswith('spark')):
         print >> sys.stderr, \
             "Enter spark master URL starting with spark://"
         exit(-1)
+
     sc = SparkContext(sys.argv[1], appName="GMM",
                       pyFiles=['GMMclustering.py', 'GMMModel.py'])
 
@@ -40,26 +40,21 @@ if __name__ == "__main__":
     k = int(sys.argv[3])
     if(len(sys.argv) == 5):
         n_iter = int(sys.argv[4])
-    model = GMMModel.trainGMM(data, k, n_iter=100)
-    responsibility_matrix,cluster_labels = GMMModel.resultPredict(model,data)
+    else:
+        n_iter = 100
+    model = GMMModel.trainGMM(data, k, n_iter)
+    responsibility_matrix, cluster_labels = GMMModel.resultPredict(model, data)
 
-    print cluster_labels.collect()
-    # # Writing the GMM components to files
-    # means_file = input_file.split(".")[0]+"/means"
-    # sc.parallelize(model.Means, 1).saveAsTextFile(means_file)
+    # Writing the GMM components to files
+    means_file = input_file.split(".")[0]+"/means"
+    sc.parallelize(model.Means, 1).saveAsTextFile(means_file)
 
-    # covar_file = input_file.split(".")[0]+"/covars"
-    # sc.parallelize(model.Covars, 1).saveAsTextFile(covar_file)
+    covar_file = input_file.split(".")[0]+"/covars"
+    sc.parallelize(model.Covars, 1).saveAsTextFile(covar_file)
 
-    # responsbilities = input_file.split(".")[0]+"/responsbilities"
-    # responsibility_matrix.coalesce(1).saveAsTextFile(responsbilities)
+    responsbilities = input_file.split(".")[0]+"/responsbilities"
+    responsibility_matrix.coalesce(1).saveAsTextFile(responsbilities)
 
-    # cluster_file = input_file.split(".")[0]+"/clusters"
-    # cluster_labels.coalesce(1).saveAsTextFile(cluster_file)
-
-    x = data.map(lambda d: ([d[0]])).collect()
-    y = data.map(lambda d: ([d[1]])).collect()
-    plt.scatter(x, y, c=cluster_labels.collect())
-    plt.show()
-    matplotlib.pyplot.close()
+    cluster_file = input_file.split(".")[0]+"/clusters"
+    cluster_labels.coalesce(1).saveAsTextFile(cluster_file)
     sc.stop()
